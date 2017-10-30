18acba0007a32ac4d9c119cbe437324150588620
Marcin <kalinowskixmarcin@o2.pl>
2017-10-31 00:18:22 +0100

    navigation desktop

--- index.php
+++ index.php
@@ -2,25 +2,32 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Pluto test project - Marcin Kalinowski</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="dist/styles.all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&amp;subset=latin-ext" rel="stylesheet">
   </head>
   <body>
 
     <nav id="main_navigation">
-      <button class="hamburger hamburger--squeeze" type="button">
-        <span class="hamburger-box"><span class="hamburger-inner"></span></span>
-      </button>
-      <ul class="main_navigation_list">
-        <li class="main_navigation_list_el"><a href="#">Home</a></li>
-        <li class="main_navigation_list_el"><a href="#">About</a></li>
-        <li class="main_navigation_list_el"><a href="#">Portfolio</a></li>
-        <li class="main_navigation_list_el"><a href="#">Blog</a></li>
-        <li class="main_navigation_list_el"><a href="#">Contact</a></li>
-      </ul>
+      <div class="wrapper wrapper--main_navigation">
+        <button class="hamburger hamburger--squeeze" type="button">
+          <span class="hamburger-box"><span class="hamburger-inner"></span></span>
+        </button>
+        <div class="main_navigation_logo">
+          <img src="img/logo_pluto.png" alt="company logo" class="main_navigation_logo_img">
+          <span>Pluto</span>
+        </div>
+        <ul class="main_navigation_list">
+          <li class="main_navigation_list_el"><a href="#">Home</a></li>
+          <li class="main_navigation_list_el"><a href="#">About</a></li>
+          <li class="main_navigation_list_el"><a href="#">Portfolio</a></li>
+          <li class="main_navigation_list_el"><a href="#">Blog</a></li>
+          <li class="main_navigation_list_el"><a href="#">Contact</a></li>
+          <li class="main_navigation_list_el main_navigation_list_el--search"><a href="#"></a></li>
+        </ul>
+      </div>
     </nav>
-    Pluto
 
     <script src="dist/scripts.all.min.js"></script>
   </body>
