410430c5903a12a3004cef9f87af30a2ec570832
Marcin <kalinowskixmarcin@o2.pl>
2017-10-30 23:01:52 +0100

    base template

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="dist/styles.all.min.css">
+  </head>
+  <body>
+
+    <script src="dist/scripts.all.min.js"></script>
+  </body>
+</html>
